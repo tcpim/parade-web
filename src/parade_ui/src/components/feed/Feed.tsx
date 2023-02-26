fd5d86cc57cf8727eb3f778d4df10911d0353c0f
tcpim <tim3wang@email.com>
2023-02-26 10:53:31 -0800

    call http request

--- src/parade_ui/src/components/feed/Feed.tsx
+++ src/parade_ui/src/components/feed/Feed.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Box } from '@mui/material';
 
 const Feed = () => {
+    const [httpRes, seHttpRes] = useState('')
+
+    useEffect(() => {
+        fetch('https://parade-api-lbbl5ziblq-wl.a.run.app/fetch')
+            .then(response => response.text())
+            .then(data => seHttpRes(data));
+    })
+
     return (
         <Box bgcolor='rgba(251, 18, 18, 0.2)' sx={{ flexBasis: '60%', marginLeft: "15%", marginTop: "5%" }}>
-            Feed
+            {httpRes}
         </Box>
     )
 }
